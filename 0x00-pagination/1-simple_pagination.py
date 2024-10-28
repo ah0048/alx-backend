f8d649954dce2e05f9bafda93ec93fff7451530e
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-10-28 19:17:30 +0300

    1

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+'''server class and index_range function'''
+import csv
+import math
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    '''return a tuple of size two containing a start index and an end index'''
+    if page <= 0 or page_size < 0:
+        return "invalid page or page size"
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        try:
+            assert isinstance(page, int)
+            assert isinstance(page_size, int)
+            assert page > 0
+            assert page_size > 0
+            dataset = self.dataset()
+            start, end = index_range(page, page_size)
+            if start >= len(dataset):
+                return []
+            return dataset[start:end]
+        except AssertionError:
+            raise AssertionError
