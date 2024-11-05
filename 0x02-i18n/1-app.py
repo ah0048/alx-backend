6efe85e77037312b5c0ac8e879de7b92f74fe282
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-11-05 23:33:06 +0200

    1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -2,10 +2,7 @@
 """ Basic Flask app, Basic Babel setup
 """
 from flask import Flask, render_template
-from flask_babel import Babel, _
-
-app = Flask(__name__)
-babel = Babel(app)
+from flask_babel import Babel
 
 
 class Config:
@@ -16,7 +13,10 @@ class Config:
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+app = Flask(__name__)
 app.config.from_object(Config)
+
+babel = Babel(app)
 
 
 @app.route('/', strict_slashes=False)
